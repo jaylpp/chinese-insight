7d84122483640e123892a8ef1808ca0f7517208f
pandaJay <liupingping@purvar.com>
2024-09-15 22:29:33 +0800

    refactor: 调整使用说明文件，调整 README.md

--- popup.js
+++ popup.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     document.getElementById('instructionsButton').addEventListener('click', function() {
-        chrome.tabs.create({url: 'https://sif8f6uboze.feishu.cn/wiki/XiVcwjfdOix1L0k1XA2cJ02Un3g?from=from_copylink'});
+        chrome.tabs.create({url: 'https://github.com/jaylpp/chinese-insight'});
     });
 });
